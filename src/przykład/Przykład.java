612689186bbcdbe1904567d9b77df3820298b665
Krzysiek <Krzysiek@LAPTOP-O1Q8TBR3>
2019-03-09 12:15:02 +0100

    Drugi Commit

--- src/przykład/Przykład.java
+++ src/przykład/Przykład.java
@@ -16,6 +16,9 @@ public class Przykład {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        
+        System.out.print("Zmiana");
     }
     
 }
